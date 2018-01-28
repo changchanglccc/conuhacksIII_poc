9eee06defc92fd42418b740d1b4c78787a979e1c
changchanglccc <lichong06091992@gmail.com>
2018-01-28 03:00:03 -0500

    add basic flask

--- poc.py
+++ poc.py
@@ -1,15 +1,30 @@
+from flask import Flask, request
+
+
 import json
 from watson_developer_cloud import VisualRecognitionV3
 
-visual_recognition = VisualRecognitionV3(
-    '2016-05-20',
-    api_key='{api_key}')
 
-with open('./fruitbowl.jpg', 'rb') as images_file:
-    classes = visual_recognition.classify(
-        images_file,
-        parameters=json.dumps({
-            'classifier_ids': ['fruits_1462128776','SatelliteModel_6242312846'],
-            'threshold': 0.6
-        }))
-print(json.dumps(classes, indent=2))
+app = Flask(__name__)
+
+
+visual_recognition = VisualRecognitionV3(
+    '2018-01-27',
+    api_key='{api_key~~~~~~~ our api_key}')
+
+classifier = visual_recognition.get_classifier(
+    classifier_id='???????????????????')
+print(json.dumps(classifier, indent=2))
+# with open('./fruitbowl.jpg', 'rb') as images_file:
+#     classes = visual_recognition.classify(
+#         images_file,
+#         parameters=json.dumps({
+#             'classifier_ids': ['fruits_1462128776','SatelliteModel_6242312846'],
+#             'threshold': 0.6
+#         }))
+# print(json.dumps(classes, indent=2))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, threaded=True)
+# host='0.0.0.0', port='80',
